25142be56e9cd15640a2d7cfb8e4e909632ce022
MOUNDHER007 <monder.nina@gmail.com>
2025-10-20 13:56:28 +0200

    Update TP01.C

--- TP01.C
+++ TP01.C
@@ -1,50 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// دالة ترجع مجموع عددين
-int add(int a, int b) {
-    return a + b;
-}
-
-// دالة ترجع العدد الأكبر بين عددين
-int max(int a, int b) {
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-
-// دالة تحسب المعدل الحسابي لمصفوفة
-float average(int arr[], int n) {
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum = add(sum, arr[i]); // نستخدم دالة add
+char* LoadString(int N) {
+    char *str = (char*) malloc((N + 1) * sizeof(char));
+    if (str == NULL) {
+        printf("Error: Memory allocation failed.
+");
+        exit(1);
     }
-    return (float)sum / n;
+    printf("Enter a string (max %d characters): ", N);
+    fgets(str, N + 1, stdin);
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == '
+') {
+            str[i] = '';
+            break;
+        }
+    }
+    return str;
+}
+
+int StringLength(char *str) {
+    int len = 0;
+    while (str[len] != '') len++;
+    return len;
+}
+
+void LoadArray(char *p, char arr[]) {
+    int i = 0;
+    while (p[i] != '') {
+        arr[i] = p[i];
+        i++;
+    }
+    arr[i] = '';
+}
+
+void ReverseArray(char arr[], char rev[], int n) {
+    for (int i = 0; i < n; i++) {
+        rev[i] = arr[n - 1 - i];
+    }
+    rev[n] = '';
+}
+
+void DisplayArray(char arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%c", arr[i]);
+    }
+    printf("
+");
+}
+
+int SumStringASCII(char *p) {
+    if (*p == '')
+        return 0;
+    return (int)(*p) + SumStringASCII(p + 1);
+}
+
+void ReverseString(char *start, char *end) {
+    if (start >= end)
+        return;
+    char temp = *start;
+    *start = *end;
+    *end = temp;
+    ReverseString(start + 1, end - 1);
 }
 
 int main() {
+    char *str;
     int n;
-    printf("Enter number of elements: ");
+    printf("Please enter the maximum size of the string:
+");
     scanf("%d", &n);
-
-    int arr[n];
-
-    printf("Enter %d integers:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // إيجاد العدد الأكبر
-    int largest = arr[0];
-    for (int i = 1; i < n; i++) {
-        largest = max(largest, arr[i]);
-    }
-
-    // حساب المعدل
-    float avg = average(arr, n);
-
-    printf("\nLargest number = %d\n", largest);
-    printf("Average = %.2f\n", avg);
-
+    getchar();
+    str = LoadString(n);
+    int len = StringLength(str);
+    char arr[len + 1], rev[len + 1];
+    LoadArray(str, arr);
+    printf("
+Original array: ");
+    DisplayArray(arr, len);
+    ReverseArray(arr, rev, len);
+    printf("Reversed array: ");
+    DisplayArray(rev, len);
+    int sum = SumStringASCII(str);
+    printf("Sum of ASCII values (recursive): %d
+", sum);
+    ReverseString(str, str + len - 1);
+    printf("String reversed recursively: %s
+", str);
+    free(str);
     return 0;
 }
